9748039aa7242d00465660bba76365bda02f6625
sman40901 <sujanmanpr@gmail.com>
2021-06-07 22:27:45 -0700

    simple password generator

--- simplePassGen/script.js
+++ simplePassGen/script.js
@@ -2,11 +2,14 @@ var az = [...'abcdefghijklmnopqrstuvwxyz'];
 var AZ = [...'ABCDEFGHIJKLMNOPQRSTUVWXYZ'];
 var num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 
-function getArrayElementRandom(arr, max, min) {
-    var r = Math.floor(
-        Math.random() * (max - min) + min
-    );
-    return arr[r];
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+
+function getArrayElementRandom(arr, max) {
+
+    return arr[getRandomInt(max)];
 }
 
 var min = 0
@@ -15,19 +18,19 @@ var last = 9
 
 function generateSimplePassword() {
     var a = (
-        getArrayElementRandom(AZ, min, max)
-        + getArrayElementRandom(az, min, max)
-        + getArrayElementRandom(az, min, max)
-        + getArrayElementRandom(az, min, max)
-        + getArrayElementRandom(az, min, last)
-        + getArrayElementRandom(num, min, last)
-        + getArrayElementRandom(AZ, min, max)
-        + getArrayElementRandom(AZ, min, max)
-        + getArrayElementRandom(AZ, min, max)
-        + getArrayElementRandom(az, min, max)
-        + getArrayElementRandom(az, min, max)
-        + getArrayElementRandom(az, min, max)
-        + getArrayElementRandom(az, min, last)
+        getArrayElementRandom(AZ, max)
+        + getArrayElementRandom(az, max)
+        + getArrayElementRandom(az, max)
+        + getArrayElementRandom(az, max)
+        + getArrayElementRandom(az, last)
+        + getArrayElementRandom(num, last)
+        + getArrayElementRandom(AZ, max)
+        + getArrayElementRandom(AZ, max)
+        + getArrayElementRandom(AZ, max)
+        + getArrayElementRandom(az, max)
+        + getArrayElementRandom(az, max)
+        + getArrayElementRandom(az, max)
+        + getArrayElementRandom(az, last)
     );
 
     document.getElementById('pass1').innerHTML = '<h2>' + a + '</h2>';
